8cba9bf7b21bd3006ad70b4b4c1de7d2d46b1077
mohamed271220 <mohamedaprahym@gmail.com>
2023-07-11 20:47:48 +0300

    Dashboard

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,9 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { createMuiTheme } from "@mui/material";
 import { themeSettings } from "./theme";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
 import React from "react";
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -14,7 +16,10 @@ function App() {
           <CssBaseline />
         </ThemeProvider>
         <Routes>
-          <Route element={<Layout />}></Route>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to={"/dashboard"} replace />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ admin/src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDownwardOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "../state";
+import { useTheme } from "@emotion/react";
+import { AppBar, Toolbar } from "@mui/material";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* leftside */}
+        
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
